f068bd911b788c9cb43db5ff652ec72cc90c081f
Emil Holm Nauerby <emil@nauerby.com>
2015-03-09 14:47:14 +0100

    Fix wrongly written test

--- unit-tests/client_test.clj
+++ unit-tests/client_test.clj
@@ -9,9 +9,9 @@
   (expect true @result))
 
 ;; Calling emit should trigger multiple handlers for the given topic.
-(let [[result1 result2 result3] [(repeatedly 3 #(promise))]
-      [handle1 handle2 handle3] [(handle :topic #(deliver result1 true))
-                                 (handle :topic #(deliver result2 true))
-                                 (handle :topic #(deliver result3 true))]]
+(let [[result1 result2 result3] [(repeatedly 3 promise)]]
+  (handle :topic #(deliver result1 true))
+  (handle :topic #(deliver result2 true))
+  (handle :topic #(deliver result3 true))
   (emit! :topic)
   (expect true (and @result1 @result2 @result3)))
