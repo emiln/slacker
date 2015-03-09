04236dc412949c22c81b58511a941dd8fb3f9b32
Emil Holm Nauerby <emil@nauerby.com>
2015-03-09 14:33:02 +0100

    How on earth did I miss those closing brackets?

--- unit-tests/client_test.clj
+++ unit-tests/client_test.clj
@@ -1,6 +1,6 @@
 (ns client-test
   (:require [expectations :refer [expect]]
-            [slacker.client :refer [emit! handle))
+            [slacker.client :refer [emit! handle]]))
 
 ;; Calling emit should trigger a handle for the given topic.
 (let [result (promise)
