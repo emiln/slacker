4bde04e1b0fce0aaa2c581f58d8f6997903f6d85
Emil Holm Nauerby <emil@nauerby.com>
2015-03-09 14:38:03 +0100

    Test multiple handlers for one emit call

--- unit-tests/client_test.clj
+++ unit-tests/client_test.clj
@@ -2,8 +2,16 @@
   (:require [expectations :refer [expect]]
             [slacker.client :refer [emit! handle]]))
 
-;; Calling emit should trigger a handle for the given topic.
+;; Calling emit should trigger a handler for the given topic.
 (let [result (promise)
       handler (handle :test #(deliver result true))]
   (emit! :test)
   (expect true @result))
+
+;; Calling emit should trigger multiple handlers for the given topic.
+(let [[result1 result2 result3] [(repeatedly 3 #(promise))]
+      [handle1 handle2 handle3] [(handle :topic #(deliver result1 true))
+                                 (handle :topic #(deliver result2 true))
+                                 (handle :topic #(deliver result3 true))]]
+  (emit! :topic)
+  (expect true (and @result1 @result2 @result3)))
