cb6cea3be3b6b7615473e4b62f0255b8654f0c7e
Emil Holm Nauerby <emil@nauerby.com>
2015-03-09 14:26:14 +0100

    Test that emit! and handle work as intended

--- unit-tests/client_test.clj
+++ unit-tests/client_test.clj
@@ -1,2 +1,9 @@
 (ns client-test
-  (:require [expectations :refer [expect]]))
+  (:require [expectations :refer [expect]]
+            [slacker.client :refer [emit! handle))
+
+;; Calling emit should trigger a handle for the given topic.
+(let [result (promise)
+      handler (handle :test #(deliver result true))]
+  (emit! :test)
+  (expect true @result))
